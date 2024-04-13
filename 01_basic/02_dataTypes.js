0a85aec4e1da2c5d21af3b3471f7ee4e909d0060
imukul.kadal <141301378+mukulkadal@users.noreply.github.com>
2024-04-13 05:16:32 +0000

    container config done

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log( 3 + 3) // code readability should be high
+
+console.log("Mukul Kadal")
+
+
+let name = "mukul"
+let age = 20
+let isLoggedIn = true
+
+// Number => 2 to power 53 (range)
+// BigInt 
+// String => ""
+// Boolean => true/false
+// null => standalone value
+// undefined => 
+// Symbol => it's use unique component
+
+// Object 
+
+console.log(typeof undefined); // It's  undefined
+console.log(typeof null); // It's object
