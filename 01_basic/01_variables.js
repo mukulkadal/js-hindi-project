03cb6279e025c9c104ba7f27d5bae5779311196f
imukul.kadal <141301378+mukulkadal@users.noreply.github.com>
2024-04-13 03:56:28 +0000

    con

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 12344
+let accountEmail = "mukulkadal@gmail.com"
+var accountPass = 12345
+accountCity = "chandighar"
+
+// accountId = 2 // not allowed
+
+accountEmail = "nehakdal@gmail.com"
+accountPass = 212121
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPass, accountCity])
