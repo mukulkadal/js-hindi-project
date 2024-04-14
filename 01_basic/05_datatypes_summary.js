c2a9f1e17b1c181218c38aec86ce810b2f2b9410
imukul.kadal <141301378+mukulkadal@users.noreply.github.com>
2024-04-14 11:27:25 +0000

    datatypes_summary

---
+++ 01_basic/05_datatypes_summary.js
@@ -0,0 +1,49 @@
+// # Primitive
+
+// 7 Types : Srting, Number, Boolearn, Null, Undefined, Symbol, BigInt
+
+
+const name = "Mukul"
+const number = 100.3
+const isLoggedIn = false
+const outSideTemp = null
+let userEmail;
+
+const id = Symbol("123")
+const anotherId = Symbol("123")
+// console.log(id === anotherId);
+
+const bigInt = 1234555633454353553n
+
+// Reference types ( Non Primitive )-
+
+// Array, Object, Function
+
+
+// Array Example
+const MyBrothes =  ["Nitin", "Sonu", "Punit" ]
+
+// console.log(MyBrothes)
+
+// Object Example...
+
+const myBio ={
+
+    name: "mukul",
+    age: 20,
+    height: "5:11",
+}
+
+// console.log(myBio)
+
+// Function. Example.....
+
+const myFunction = function(){
+
+    console.log("Hello Mukul Kadal")
+}
+myFunction()
+
+
+
+
