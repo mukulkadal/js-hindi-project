32daca5f0991da77246e044bd0c02721dd9f2144
imukul.kadal <141301378+mukulkadal@users.noreply.github.com>
2024-04-14 05:54:10 +0000

    conversion__Operation_done

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -24,6 +24,45 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
  let stringNumber = String(someNumber)
- console.log(stringNumber)
- console.log(typeof stringNumber);
+//  console.log(stringNumber)
+//  console.log(typeof stringNumber);
+
+
+// ************************** Operation **********************************
+
+let value = 3
+
+let negValue =  -value
+
+// console.log(negValue)
+
+let str1 = "Hello"
+let str2 = " Mukul kadal"
+let str3 = str1 + str2
+
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2 + 2);
+// console.log(3+ 4 * 5 % 3)
+
+// console.log(true);
+// console.log(+true);
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+// console.log(num3);
+
+let gameCounter = 100
+
+--gameCounter;
+
+console.log(gameCounter);
+
+
+
+
 
