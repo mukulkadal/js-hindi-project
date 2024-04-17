e98793778653b5f2cfc7b99fbf0e266c30245114
imukul.kadal <141301378+mukulkadal@users.noreply.github.com>
2024-04-17 08:19:30 +0000

    Nums __maths

---
+++ 01_basic/07_nums_maths.js
@@ -0,0 +1,15 @@
+const score = 400
+console.log(score); // or ye simple number hai bs // ans ==> 400; 
+
+const balance = new Number(100)
+console.log(balance) // ans => iska mtlab hai ki ye spacial number hi hai <==> [Number: 100]
+
+
+console.log(balance.toString()) // hum number ko string main bhi change kr sakte hai or lenght bhi check kr sakte hai// ans => 100 //// lenght <==> 3
+console.log(balance.toFixed(2)) // ans => 100.00 
+
+
+const otherNumber = 123.89657
+console.log(otherNumber.toPrecision(4)) // ans => 123.9
+
+
